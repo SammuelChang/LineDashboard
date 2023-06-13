d8b6feb431a6c7b234c5bbbc3e7dc7a618a0b7d2
SammuelChang <s23593517844@hotmail.com>
2023-06-14 00:46:56 +0800

    fix: 調整教學圖片不正確顯示問題

--- src/pages/Steps/index.tsx
+++ src/pages/Steps/index.tsx
@@ -1,7 +1,8 @@
+import steps from "../../assets/steps.png";
 function Steps() {
   return (
     <div className="p-10 flex justify-center">
-      <img src="../src/assets/steps2.png" width="300" />
+      <img src={steps} width="300" />
     </div>
   );
 }
